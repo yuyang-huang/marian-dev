92f61c61150ac0c4887cfdd5ae8c894b8e36b5ff
Yu-Yang Huang <yuyang.gnauh@gmail.com>
2020-04-22 12:03:51 +0800

    Implement --transformer-depth-scaling

--- src/layers/generic.h
+++ src/layers/generic.h
@@ -420,11 +420,11 @@ public:
 
 // like affine() but with built-in parameters, activation, and dropout
 static inline
-Expr denseInline(Expr x, std::string prefix, std::string suffix, int outDim, const std::function<Expr(Expr)>& actFn = nullptr, float dropProb = 0.0f)
+Expr denseInline(Expr x, std::string prefix, std::string suffix, int outDim, const std::function<Expr(Expr)>& actFn = nullptr, float dropProb = 0.0f, float initScalingFactor = 1.0f)
 {
   auto graph = x->graph();
 
-  auto W = graph->param(prefix + "_W" + suffix, { x->shape()[-1], outDim }, inits::glorotUniform());
+  auto W = graph->param(prefix + "_W" + suffix, { x->shape()[-1], outDim }, inits::glorotUniform(true, true, initScalingFactor));
   auto b = graph->param(prefix + "_b" + suffix, { 1,              outDim }, inits::zeros());
 
   x = affine(x, W, b);
